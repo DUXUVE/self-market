20f970d7369d31d8fdc738eeced05d3a7e033eeb
kaz1nak666 <impreezzza@gmail.com>
2026-08-17 21:16:27 +0300

    регистрация

---
+++ app/models/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import BigInteger, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, autoincrement=True
+    )
+    phone_number: Mapped[str] = mapped_column(String(31), nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    city: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar_link: Mapped[str] = mapped_column(String(1023), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
