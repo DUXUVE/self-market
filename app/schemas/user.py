671132853fd279ba7aeba52b22913451454885d5
DUXUVE <m.milana2007.mil@gmail.com>
2026-09-23 18:12:12 +0300

    2

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,15 +1,24 @@
-from pydantic import BaseModel, PhoneNumber
+from pydantic import BaseModel
+from pydantic_extra_types.phone_numbers import PhoneNumber
 
-class UserCreate:
+
+class UserCreate(BaseModel):
     phone_number: PhoneNumber
     password: str
     name: str
     city: str
-    description: str | None
+    description: str | None = None
 
-class User:
+
+class User(BaseModel):
     id: int
     is_active: bool = True
 
     class Config:
         from_attributes = True
+
+
+class UserInDB(User):
+    username: str
+    email: str
+    hashed_password: str
